02d25f1ba2e838644fbc93723931e3c5fb7748d4
Kristian Zelenko <77212838+kristianzelenko@users.noreply.github.com>
2021-03-28 17:18:20 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+/*
+-------------------АКОРДИОН-------------------
+*/
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -16,7 +19,9 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-
+/*
+-------------------НАВБАР-------------------
+*/
 $(document).ready(function () {
   $("a").click(function () {
       var elementClick = $(this).attr("href");
@@ -48,4 +53,78 @@ menu_list.onclick = function() {
 };
 
 
+/*
+-------------------ФОРМА-------------------
+*/
 
+var form = $('#contact'),
+    submit = form.find('[name="submit"]');
+
+form.on('submit', function(e) {
+  e.preventDefault();
+  
+  // avoid spamming buttons
+  if (submit.attr('value') !== 'Send')
+    return;
+  
+  var valid = true;
+  form.find('input, textarea').removeClass('invalid').each(function() {
+    if (!this.value) {
+      $(this).addClass('invalid');
+      valid = false;
+    }
+  });
+  
+  if (!valid) {
+    form.animate({left: '-3em'},  50)
+        .animate({left:  '3em'}, 100)
+        .animate({left:    '0'},  50);
+  } else {
+    submit.attr('value', 'Sending...')
+          .css({boxShadow: '0 0 200em 200em rgba(225, 225, 225, 0.6)',
+                backgroundColor: '#ccc'});
+    // simulate AJAX response
+    setTimeout(function() {
+      // step 1: slide labels and inputs
+      // when AJAX responds with success
+      // no animation for AJAX failure yet
+      form.find('label')
+          .animate({left: '100%'}, 500)
+          .animate({opacity: '0'}, 500);
+    }, 1000);
+    setTimeout(function() {
+      // step 2: show thank you message after step 1
+      submit.attr('value', 'Thank you :)')
+            .css({boxShadow: 'none'});
+    }, 2000);
+    setTimeout(function() {
+      // step 3: reset
+      form.find('input, textarea').val('');
+      form.find('label')
+          .css({left: '0'})
+          .animate({opacity: '1'}, 500);
+      submit.attr('value', 'Send')
+            .css({backgroundColor: ''});
+    }, 4000);
+  }
+});
+
+$(function(){
+  $('.minimized').click(function(event) {
+    var i_path = $(this).attr('src');
+    $('body').append('<div id="overlay"></div><div id="magnify"><img src="'+i_path+'"><div id="close-popup"><i></i></div></div>');
+    $('#magnify').css({
+     left: ($(document).width() - $('#magnify').outerWidth())/2,
+     // top: ($(document).height() - $('#magnify').outerHeight())/2 upd: 24.10.2016
+            top: ($(window).height() - $('#magnify').outerHeight())/2
+   });
+    $('#overlay, #magnify').fadeIn('fast');
+  });
+  
+  $('body').on('click', '#close-popup, #overlay', function(event) {
+    event.preventDefault();
+    $('#overlay, #magnify').fadeOut('fast', function() {
+      $('#close-popup, #magnify, #overlay').remove();
+    });
+  });
+});
